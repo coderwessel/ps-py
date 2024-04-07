8a7589933c0f08f040d9d0f79b1cd157bd903662
coderwessel <39902558+coderwessel@users.noreply.github.com>
2024-04-07 17:55:04 +0200

    First working version

---
+++ namecaller.py
@@ -0,0 +1,18 @@
+import argparse
+from art import text2art
+
+parser = argparse.ArgumentParser(
+		description			= "python %(prog)s piet, calls out piet",
+		epilog				= '',
+		add_help			= True
+	)
+parser.add_argument('--version', action='version', version='%(prog)s 0.001')
+parser.add_argument('name', nargs='?')
+args = parser.parse_args()
+
+if args.name==None:
+    name = input("Enter your name: ")
+else:
+    name=args.name
+print(text2art("Hello, " + name))        
+
